91b14414764d9d51dc1116a394818aed36c8b896
Chike <chikespott@gmail.com>
2016-12-03 10:42:13 +0100

    create retrofit client

---
+++ app/src/main/java/com/chikeandroid/retrofittutorial/data/remote/RetrofitClient.java
@@ -0,0 +1,24 @@
+package com.chikeandroid.retrofittutorial.data.remote;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Chike on 12/3/2016.
+ */
+
+public class RetrofitClient {
+
+    public static final String BASE_URL = "http://jsonplaceholder.typicode.com";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
